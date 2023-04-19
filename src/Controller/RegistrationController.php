22b84ee791cb6c95e675f3d8ecc7d4d2113f0c7f
melfi <melfi@laptop-mohammed.com>
2023-04-19 13:21:18 +0200

    Document the API with Nelmio

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -28,7 +28,7 @@ class RegistrationController extends AbstractController
         $this->emailVerifier = $emailVerifier;
     }
 
-    #[Route('/api/register', name: 'app_register')]
+    #[Route('/api/register', name: 'app_register', methods: ['POST'])]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher,
     UserRepository $userRepository, SerializerInterface $serializer, UrlGeneratorInterface $urlGenerator ): JsonResponse
     {
